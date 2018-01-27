1929276bd6727a566d01c3e11947218590495a01
Mr Ruba <zacharyjcanderson@gmail.com>
2018-01-27 12:10:29 -0800

    Added the capture of audio

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+/*
+=====================================================================
+  Purpose: Creates a server application for the backend of my-eyes.
+=====================================================================
+*/
+
 require('dotenv').load();
 const crypto = require("crypto");
 const mime = require("mime");
@@ -9,46 +15,21 @@ const TextToSpeechV1 = require('watson-developer-cloud/text-to-speech/v1');
 const fs = require('fs');
 const multer  = require('multer')
 
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads/')
-  },
-  filename: function (req, file, cb) {
-    crypto.pseudoRandomBytes(16, function (err, raw) {
-      cb(null, raw.toString('hex') + Date.now() + '.' + mime.getExtension(file.mimetype));
-    });
-  }
-});
-var upload = multer({ storage: storage,
-    limits: { fileSize: 1024 * 1024 * 50}
-});
+/*
+=====================================================================
+     SETUP
+       - Create Client for google Vision
+       - Create text param IBM text-to-Speech
+       - Define directories for multer to save files
+         - Image uploads
+         - Text audio
+=====================================================================
+*/
 
-const app = express();
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-
-// Creates a client
+// Setup for Goolge Vision API - Create a client from my-eyes .json file
 const client = new vision.ImageAnnotatorClient();
 
-// Performs text detection on the local file
-const convert_image_to_text = (fileName, res) => {
-    client
-        .documentTextDetection(fileName)
-        .then(results => {
-            const fullTextAnnotation = results[0].fullTextAnnotation;
-            return fullTextAnnotation.text;
-        })
-        .then(text => {
-            var transcript = text_to_speech.synthesize(text_params(text));
-            transcript.pipe(res);
-        })
-        .catch(err => {
-            console.error('ERROR:', err);
-        });
-}
-
+// Setup for IBM text-to-speech - Create text_params with user and password
 const text_to_speech = new TextToSpeechV1({
     username: process.env.USERNAME_WAT,
     password: process.env.PASSWORD_WAT
@@ -61,18 +42,96 @@ const text_params = (text_to_speech) => {
     accept: 'audio/mp3'
 }};
 
+//Set up for Storing Images under upload folder
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/')
+  },
+  filename: function (req, file, cb) {
+    crypto.pseudoRandomBytes(16, function (err, raw) {
+      cb(null, raw.toString('hex') + Date.now() + '.' + mime.getExtension(file.mimetype));
+    });
+  }
+});
+
+var upload = multer({ storage: storage,
+    limits: { fileSize: 1024 * 1024 * 50}
+});
+
+//Set up for Storing Audio under the audio folder
+var storage2 = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './textAudio/')
+  },
+  filename: function (req, file, cb) {
+    crypto.pseudoRandomBytes(16, function (err, raw) {
+      cb(null, raw.toString('hex') + Date.now() + '.' + mime.getExtension(file.mimetype));
+    });
+  }
+});
+
+var textAudio = multer({ storage: storage2,
+    limits: { fileSize: 1024 * 1024 * 50}
+});
+
+/*
+=====================================================================
+     FUNCTIONS
+=====================================================================
+*/
+
+// Performs text detection on the local file
+const convert_image_to_text = (fileName, audioFile, res) => {
+    client
+        .documentTextDetection(fileName) //Call google vision api on a file.
+        .then(results => {
+            const fullTextAnnotation = results[0].fullTextAnnotation; //On response from google, grab the text
+            return fullTextAnnotation.text; //We might be able to pipe this...
+        })
+        .then(text => {
+            var transcript = text_to_speech.synthesize(text_params(text)); //Once we have the text, call ibm text-to-speech and pass the text
+            transcript.pipe(audioFile.stream); //pipe the stream from ibm into our res variable (AKA, sends an audio stream)
+            transcript.pipe(res);
+        })
+        .catch(err => {
+            console.error('ERROR:', err);
+        });
+}
+
+function saveAudio(){
+  var file = fs.createWriteStream()
+  return file;
+};
+
+/*
+=====================================================================
+    APPLICATION
+=====================================================================
+*/
+const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(express.static(__dirname + '/views'));
+
+//When we get a post request with action upload, save the file, then respond with the audio: filename
 app.post('/upload', upload.single('file_input'), function (req, res, next) {
     console.log(req.file.filename);
     res.json({'audio': req.file.filename});
 })
 
-app.get('/audio/:img', function(req, res, next) {
-    convert_image_to_text("uploads/"+req.params.img, res);
+//On a get request with action audio/:img, convert the image to audio
+app.get('/audio/:img',
+  function(req, res, next) {
+    const myFileName = req.params.img + ".mp3";
+    var audioFile = {stream: fs.createWriteStream("audio/"+myFileName)};
+    convert_image_to_text("uploads/"+req.params.img, audioFile, res);
+    //audioFile.stream.end();
 });
 
-app.use(express.static(__dirname + '/views'));
+var port = process.env.PORT || 3000;
 
-var port = process.env.PORT || 3000
 app.listen(port, function() {
     console.log("To view your app, open this link in your browser: http://localhost:" + port);
 });
--- views/webcam.js
+++ views/webcam.js
@@ -1,8 +1,15 @@
+/*
+  Submits form data and then waits for a response.  Then assign the Audio
+  contols src to the response back from the server.
+*/
+
 const submitFile = (file) => {
     var formData = new FormData();
 
     formData.append("file_input", file);
 
+    //This is the actual function that sends a req
+    //to the server.
     fetch("/upload", {
         method: "POST",
         body: formData
@@ -10,23 +17,32 @@ const submitFile = (file) => {
     .then(response => response.json())
     .catch(error => console.log(error))
     .then(response => {
+      /*grab the response's json object and set src of controls to
+      audio/name-of-the-audio */
         $("#audio-controls").attr("src", "/audio/" + response.audio)
     });
 }
 
+//Simulates a click on for input data on an image
 const daisy = () => {
-    //$('#imgForm').submit();
     $("#capture-btn").click();
 }
 
+/*
+  When the page is loaded, set a callback function for when the form input
+  changes.  When it changes, compress file then submit the form data.
+*/
 $("document").ready(function(){
     $("#capture-btn").change(function(e) {
+        //Check that a file is has been loaded
         const file = e.target.files[0];
         if (!file) { return; }
 
+        //Compress the file
         new ImageCompressor(file, {
             quality: .6,
             success(result) {
+                //When compressed, submit the file
                 submitFile(result);
             },
             error(e) {
